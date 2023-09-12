446d24908d963a2aba15532d74810e77c846a53c
Nguyen The Hien <hiennt200210@gmail.com>
2023-09-12 14:35:06 +0700

    wip: Demo unit test

---
+++ mf1737-nthien/aspnetcore/DemoAspNetCore/DemoAspNetCore.UnitTests/CalculatorTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAspNetCore.UnitTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        [TestCase(1, 2, 3)]
+        [TestCase(2, 3, 5)]
+        [TestCase(3, 4, 7)]
+        public void Add_ValidInput_Sum2Digit(int x, int y, int expected)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var actual = calculator.Add(x, y);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(1, 2, -1)]
+        [TestCase(2, 3, -1)]
+        [TestCase(3, 4, -1)]
+        public void Sub_ValidInput_Sub2Digit(int x, int y, int expected)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var actual = calculator.Sub(x, y);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(1, 2, 2)]
+        [TestCase(2, 3, 6)]
+        [TestCase(3, 4, 12)]
+        public void Mul_ValidInput_Mul2Digit(int x, int y, int expected)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var actual = calculator.Mul(x, y);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(1, 2, 0)]
+        [TestCase(2, 3, 0)]
+        [TestCase(3, 4, 0)]
+        public void Div_ValidInput_Div2Digit(int x, int y, int expected)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var actual = calculator.Div(x, y);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ mf1737-nthien/aspnetcore/DemoAspNetCore/DemoAspNetCore/Calculator.cs
@@ -0,0 +1,74 @@
+﻿namespace DemoAspNetCore
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// Tính 2 số nguyên
+        /// </summary>
+        /// 
+        /// <param name="a">Số nguyên thứ nhất</param>
+        /// <param name="b">Số nguyên thứ hai</param>
+        /// 
+        /// <returns>
+        /// Tổng 2 số nguyên
+        /// </returns>
+        /// 
+        /// CreatedBy: hiennt200210 (12/09/2023)
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        /// <summary>
+        /// Tính hiệu 2 số nguyên
+        /// </summary>
+        /// 
+        /// <param name="a">Số nguyên thứ nhất</param>
+        /// <param name="b">Số nguyên thứ hai</param>
+        /// 
+        /// <returns>
+        /// Hiệu 2 số nguyên
+        /// </returns>
+        /// 
+        /// CreatedBy: hiennt200210 (12/09/2023)
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+
+        /// <summary>
+        /// Tính tích 2 số nguyên
+        /// </summary>
+        /// 
+        /// <param name="x">Số nguyên thứ nhất</param>
+        /// <param name="y">Số nguyên thứ hai</param>
+        /// 
+        /// <returns>
+        /// Tích 2 số nguyên
+        /// </returns>
+        /// 
+        /// CreatedBy: hiennt200210 (12/09/2023)
+        public int Mul(int x, int y)
+        {
+            return x * y;
+        }
+
+        /// <summary>
+        /// Tính thương 2 số nguyên
+        /// </summary>
+        /// 
+        /// <param name="x">Số nguyên thứ nhất</param>
+        /// <param name="b">Số nguyên thứ hai</param>
+        /// 
+        /// <returns>
+        /// Thương 2 số nguyên
+        /// </returns>
+        /// 
+        /// CreatedBy: hiennt200210 (12/09/2023)
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
